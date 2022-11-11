7d8724e290282d70feba0ae8491c405de57ed19d
cescoferraro <francescoaferraro@gmail.com>
2022-11-11 14:18:24 -0300

    fix deploy

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -29,6 +29,8 @@ const NotFoundPage: React.FC<PageProps> = () => {
       <h1 style={headingStyles}>Page not found</h1>
       <p style={paragraphStyles}>
         Sorry 😔, we couldn’t find what you were looking for.
+        Sorry 😔, we couldn’t find what you were looking for.
+        Sorry 😔, we couldn’t find what you were looking for.
         <br />
         {process.env.NODE_ENV === "development" ? (
           <>
